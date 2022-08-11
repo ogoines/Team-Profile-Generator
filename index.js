c6f9dee0ad8b9b192a41241c85846845b50bf913
OGrunner <orshagoines@gmail.com>
2022-08-11 02:40:01 -0400

    modified:   index.js     changed team function

--- index.js
+++ index.js
@@ -1,48 +1,111 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
 
 const team= [];
 
-const addEmployee = () => inquirer.prompt([
+const makeTeam = () => inquirer.prompt([
     {
-     name: "name",
-     message: "Please enter an employee's name:"
-    },
-    {
-     name: "id" ,
-     type: "input",
-     message: "Please enter the employees id:"  
-    },
-    {
-     name: "email",   
-     type: "input",
-     message: "What is the employee's email address?"
-    },
-    {
-     name: "position",   
+     name: "role",   
      type: "list",
      message: "What is the employee's job position?",
      choices: ["Engineer", "Intern", "Manager"]
-    },
-    {
-     name: "continue",
-     type: "list",
-     message: "Do you wish to add another employee?",
-     choices: ["yes", "no"]
     }
-])
-.then(function({backgroundInfo, continue}) {
-    let employee;
-    switch (position){
+])   
+.then(function (choice) {
+     switch(choice.role) {
         case "Engineer":
-        employee = new Engineer(name, id, email, backgroundInfo);
-        case "Intern":
-        employee = new Intern(name, id, email, backgroundInfo);
-        case "Manager":
-        employee = new Manager(name, id, email, backgroundInfo);
+        addEngineer();
+        break;    
      }
-     team.push(employee);
+     switch(choice.role){
+        case "Intern":
+        addIntern();
+        break;
+     }
+     switch(choice.role){
+        case "Manager":
+        addManager();
+        break;
+     }
+})     
+
+
+
+    
+makeTeam()  
+    
+    
+    
+    
+    
+  //  {
+  //   name: "name",
+  //   type: "input",  
+ //    message: "Please enter an employee's name"
+ //   },
+ //   {
+  //   name: "id" ,
+  //   type: "input",
+  //   message: "Please enter the employees id:"  
+  //  },
+  //  {
+  ///   name: "email",   
+   //  type: "input",
+   //  message: "What is the employee's email address?"
+  //  },
+    
+    
+    
+ //   {
+  //   name: "continue",
+  //   type: "list",
+  //   message: "Do you wish to add another employee?",
+  //   choices: ["yes", "no"]
+  //  }
+//])
+
+ //.then(function({role}){
+ //   let backgroundInfo = "";
+  //  switch (position){
+  //     case "Engineer":
+  //       backgroundInfo = "GitHub username";
      
+  //    case "Intern":
+ //        backgroundInfo = "school name";
+ //       break;
+ //      case "Manager":
+  //       backgroundInfo = "office phone number";
+  //      break;
+  // }
+ // })
+
+
+  
+
+//makeTeam()
+
+//.then(() => console.log('Successful html write'))
+
+//.catch((err) => console.error(err));
+
+//.then(function({backgroundInfo,continue}) {
+//  let employee;
+//  switch (position){
+ //     case "Engineer":
+ //        employee = new Engineer(name, id, email, backgroundInfo);
+ //        break;
+ //     case "Intern":
+  //       employee = new Intern(name, id, email, backgroundInfo);
+  //       break;
+  //    case "Manager":
+   //      employee = new Manager(name, id, email, backgroundInfo);
+  //       break;
+  //  }
+// })
+// team.push(employee);
+ //.then(function({name,position,id,email}){
+
+    //.then(function({continue})
