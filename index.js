2239261ad8970f4ca996e60860eb5feab14381f2
OGrunner <orshagoines@gmail.com>
2022-08-15 12:11:01 -0400

    modified index.js fixed bugs in switch

--- index.js
+++ index.js
@@ -3,202 +3,122 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
+//const { isTypedArray } = require("util/types");
 const team = [];
 
 const makeTeam = () => inquirer.prompt([
-    {
+   {
+      name: "name",
+      type: "input",  
+      message: "Please enter an  employee's name:"
+   },
+   {
       name: "role",   
       type: "list",
       message: "What is the employee's job position?",
       choices: ["Engineer", "Intern", "Manager"]
-    },
-   
+   },
+   {
+      name: "id" ,
+      type: "input",
+      message: "What is the employees's id?"  
+   },
+   {
+      name: "email",   
+      type: "input",
+      message: "Please enter the employees's email address:"
+   } 
 ])  
-.then(function (entry) {
-     switch(entry.role) {
-        case "Engineer":
-          addEngineer();
-        break;    
-     }
-     switch(entry.role) {
-        case "Intern":
-          addIntern();
-        break;
-     }
-     switch(entry.role) {
-        case "Manager":
-          addManager();
-        break;
-     }
-})     
+.then(function ({role, name, id, email} ) {
+      
+   switch(role) {
+      case "Engineer":
+          inquirer.prompt([
+           {
+             name: "github",
+             type: "input",
+             message: "Please enter Github user name:"
+           }
+           ])
+         .then (function({github}){
+             const engineer = new Engineer(name, id, email, github);
+             team.push(engineer);
+             console.log(team);
+             console.log("Trying to use Engineer function")
+          })   
+     break; 
+     case "Intern":
+          inquirer.prompt([
+           {
+             name: "schoolname",
+             type: "input",
+             message: "Please enter employee's school name:"
+           }
+          ])
+         .then (function({schoolname}){
+             const intern = new Intern(name, id, email, schoolname);
+             team.push(intern);
+             console.log(team);
+             console.log("Trying to use Intern function")
+          })   
+     break; 
+     case "Manager":
+      inquirer.prompt([
+      {
+        name: "officenumber",
+        type: "input",
+        message: "Please enter employee's office number:"
+      }
+      ])
+        .then (function({officenumber}){
+            const manager = new Intern(name, id, email, officenumber);
+            team.push(manager);
+            console.log(team);
+            console.log("Trying to use Manager function")
+        })   
+      break;
+
+
+   }
+
+
+}) 
+   //  switch(role) {
+     //   case "Intern":
+     //     addIntern();
+     //   break;
+    // }
+   //  switch(role) {
+   //     case "Manager":
+    //      addManager();
+    //    break;
+   //  }
+ 
 
 
 
 
+
+
+
+     
+     
+     //addEmployee();    
 
 
 
 
 function addEngineer() {
-   inquirer.prompt([
-    {
-      name: "name",
-      type: "input",  
-      message: "Please enter an Engineer's name:"
-   },
-   {
-      name: "id" ,
-      type: "input",
-      message: "What is the Engineer's id?"  
-   },
-   {
-      name: "email",   
-      type: "input",
-      message: "Please enter the Engineer's email address:"
-   }, 
-   {
-      name: "github",
-      type: "input",
-      message: "What is the Engineer's GitHub username?"
-   },
-
-  ]) .then(entry=> {
-      const engineer = new Engineer(entry.name, entry.id, entry.email, entry.github);
+          const engineer = new Engineer(entry.name, entry.id, entry.email, entry.github);
           team.push(engineer);
           console.log(team);
           console.log("Trying to addEngineer function")
           
-      });
+
 }
 
 
-function addIntern(){
-   inquirer.prompt([
-      {
-        name: "name",
-        type: "input",  
-        message: "Please enter an Intern's name:"
-     },
-     {
-        name: "id" ,
-        type: "input",
-        message: "What is the Intern's id?"  
-     },
-     {
-        name: "email",   
-        type: "input",
-        message: "Please enter the Intern's email address:"
-     }, 
-     {
-        name: "schoolname",
-        type: "input",
-        message: "What is the Intern's school name?"
-     },
-  
-    ]) .then(entry=> {
-        const intern = new Intern(entry.name, entry.id, entry.email, entry.schoolname);
-            team.push(intern);
-            console.log(team);
-            console.log("Trying to addIntern function")
-            
-        });
-  }
-   
-
-  function addManager(){
-   inquirer.prompt([
-      {
-        name: "name",
-        type: "input",  
-        message: "Please enter a Manager's name:"
-     },
-     {
-        name: "id" ,
-        type: "input",
-        message: "What is the Manager's id?"  
-     },
-     {
-        name: "email",   
-        type: "input",
-        message: "Please enter the Manager's email address:"
-     }, 
-     {
-        name: "officenumber",
-        type: "input",
-        message: "What is the Manager's office number ?"
-     },
-  
-    ]) .then(entry=> {
-        const manager = new Intern(entry.name, entry.id, entry.email, entry.officenumber);
-            team.push(manager);
-            console.log(team);
-            console.log("Trying to addManager function")
-            
-        });
-  }
-   
-   
-   
-   
-   
    
 makeTeam()  
     
     
-    
-    
-    
-   
-   
-    
-    
- //   {
-  //   name: "continue",
-  //   type: "list",
-  //   message: "Do you wish to add another employee?",
-  //   choices: ["yes", "no"]
-  //  }
-//])
-
- //.then(function({role}){
- //   let backgroundInfo = "";
-  //  switch (position){
-  //     case "Engineer":
-  //       backgroundInfo = "GitHub username";
-     
-  //    case "Intern":
- //        backgroundInfo = "school name";
- //       break;
- //      case "Manager":
-  //       backgroundInfo = "office phone number";
-  //      break;
-  // }
- // })
-
-
-  
-
-
-
-//.then(() => console.log('Successful html write'))
-
-//.catch((err) => console.error(err));
-
-//.then(function({backgroundInfo,continue}) {
-//  let employee;
-//  switch (position){
- //     case "Engineer":
- //        employee = new Engineer(name, id, email, backgroundInfo);
- //        break;
- //     case "Intern":
-  //       employee = new Intern(name, id, email, backgroundInfo);
-  //       break;
-  //    case "Manager":
-   //      employee = new Manager(name, id, email, backgroundInfo);
-  //       break;
-  //  }
-// })
-// team.push(employee);
- //.then(function({name,position,id,email}){
-
-    //.then(function({continue})
