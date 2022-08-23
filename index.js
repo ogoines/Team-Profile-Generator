a7214fe50c03166857a9ba1ff52b9ff0d34fd406
OGrunner <orshagoines@gmail.com>
2022-08-22 23:57:20 -0400

    modified index.js added comments

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
-//const { isTypedArray } = require("util/types");
 const team = [];
 
+//Displays user prompt to add employees
 const  moreQuestions = () => inquirer.prompt([
     {
       type: "list",
@@ -23,7 +23,8 @@ const  moreQuestions = () => inquirer.prompt([
     }
  })
   
-const buildTeam = () => inquirer.prompt([
+//Displays prompts to add employee info
+ const buildTeam = () => inquirer.prompt([
    {
       name: "name",
       type: "input",  
@@ -47,7 +48,7 @@ const buildTeam = () => inquirer.prompt([
    } 
 ])  
 .then(function ({role, name, id, email}) {
- 
+//prompts user input 
  if (role === "Engineer") {
          inquirer.prompt([
            {
@@ -57,7 +58,7 @@ const buildTeam = () => inquirer.prompt([
            }
          ])
          .then (function({github}) {
-         
+            // creates Engineer info adds to html
              const employee = new Engineer(name, id, email, github);
              team.push(employee);
              engineerHtml(name, id, email, github);
@@ -74,7 +75,8 @@ const buildTeam = () => inquirer.prompt([
            }
           ])
          .then (function({schoolname}) {
-             const employee  = new Intern(name, id, email, schoolname);
+             // creates Intern info adds to html 
+            const employee  = new Intern(name, id, email, schoolname);
              team.push(employee);
              internHtml(name,id,email,schoolname);
              moreQuestions();
@@ -89,6 +91,7 @@ const buildTeam = () => inquirer.prompt([
           }
          ])
         .then (function({officenumber}) {
+             // creates Manager info adds to html 
              const employee = new Manager (name, id, email, officenumber);
              team.push(employee);
              managerHtml(name, id, email, officenumber);
@@ -97,19 +100,23 @@ const buildTeam = () => inquirer.prompt([
       
       }
 })      
-
+//uses string to create header then writes to index.html 
 function headerHtml(){
  const headerData =
 `<!DOCTYPE html>
   <html lang="en">
   <head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-     <link rel="stylesheet" href="dist/style.css">
-     <title>Team Profile Generator</title>
-   </head>`;
+     <meta charset="UTF-8" />
+     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+     <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+      crossorigin="anonymous"/>
+    <link rel="stylesheet" href="dist/style.css">
+    <title>Team Profile Generator</title>
+</head>`;
  
   fs.writeFile("index.html", headerData, function(err) {
      if (err) {
@@ -119,24 +126,35 @@ function headerHtml(){
   });
 }
 headerHtml(); 
-
+//creates body of html then appends to header
 function bodyHtml(){
    let teamData = `<body>
-   <nav class="navbar navbar-dark bg-dark justify-content-center align-items-center">
-       <span class="navbar-brand mb-0 h1"><h1>My Team</h1></span>
-   </nav>
-   <div class="row">`;
+   <div class="container">
+       <div class="jumbotron text-center">
+           <div class="container">
+               <h1 class="display-2">My Team</h1>
+          </div>
+       </div>
+   </div>
+
+   <div class="container">
+       <div class="row p-4 justify-content-center">
+           <div class="row p-3 d-flex justify-content-between">`;
+
 
    fs.appendFile("index.html", teamData, function(err) {
       if (err) {
         console.log(err);
       }
    });
-   
+   //prompts user then generates html cards with team members, adds to index.html
    buildTeam();
-   
+   //generates end of html
    let endData = 
      `</div>
+       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/	bootstrap.bundle.min.js"
+         integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+         crossorigin="anonymous"></script>
     </body>
    </html>`;
 
@@ -150,23 +168,21 @@ function bodyHtml(){
 bodyHtml();
 
 
-
-   function engineerHtml(name, id, email, gitHub) {  
-      let engineerData = 
-      `<div class="card bg-dark justify-content-center align-items-center">
-         <div class="col card-header">
-           <h4>${name}</h4>
-       </div>
-         <div class="col card-header">
-          <h4>Engineer</h4>
-       </div>
-       <ul class="list-group list-group-flush text">
-           <li class="list-group-item">ID: ${id}</li>
-           <li class="list-group-item">Email: ${email}</li>
-           <li class="list-group-item">GitHub: ${gitHub}</li>
-       </ul>
-       </div>`;  
-  
+//creates engineer card with data for html
+function engineerHtml(name, id, email, gitHub) {  
+    let engineerData = 
+      `<div class="card">
+            <div class="card-body">
+                <h3>${name}</h3>
+                <h5 class="card-subtitle">Engineer</h5>
+                <ul class="list-group list-group-flush text">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">GitHub: ${gitHub}</li>
+                </ul>
+            </div>
+        </div>`;  
+          
    fs.appendFile("index.html", engineerData, function(err) {
        if (err) {
          console.log(err);
@@ -174,22 +190,20 @@ bodyHtml();
    });
   }
 
-
+//creates intern card with data for html
 function internHtml(name, id, email, school) {  
     let internData =
-    `<div class="card bg-dark justify-content-center align-items-center">
-       <div class="col card-header">
-         <h4>${name}</h4>
-       </div>
-       <div class="col card-header">
-          <h4>Intern</h4>
-       </div>
-       <ul class="list-group list-group-flush text">
-          <li class="list-group-item">ID: ${id}</li>
-          <li class="list-group-item">Email: ${email}</li>
-          <li class="list-group-item">School Name: ${school}</li>
-       </ul>
-    </div>`;  
+    `<div class="card">
+            <div class="card-body">
+                <h3>${name}</h3>
+                <h5 class="card-subtitle">Intern</h5>
+                <ul class="list-group list-group-flush text">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">School: ${school}</li>
+                </ul>
+            </div>
+        </div>`;  
 
     fs.appendFile("index.html", internData, function(err) {
       if (err) {
@@ -197,22 +211,20 @@ function internHtml(name, id, email, school) {
      }
     });
 }
-
+//creates manager card with data for html
 function managerHtml(name, id, email, officenumber) {  
      let managerData =
-     `<div class="card bg-dark justify-content-center align-items-center">
-         <div class="col card-header">
-           <h4>${name}</h4>
-       </div>
-         <div class="col card-header">
-          <h4>Manager</h4>
-       </div>
-       <ul class="list-group list-group-flush text">
-           <li class="list-group-item">ID: ${id}</li>
-           <li class="list-group-item">Email: ${email}</li>
-           <li class="list-group-item">Office #: ${officenumber}</li>
-       </ul>
-       </div>`;  
+     `<div class="card">
+            <div class="card-body">
+                <h3>${name}</h3>
+                <h5 class="card-subtitle">Manager</h5>
+                <ul class="list-group list-group-flush text">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: ${email}</li>
+                    <li class="list-group-item">Office#: ${officenumber}</li>
+                </ul>
+            </div>
+        </div>`;  
 
      fs.appendFile("index.html", managerData, function(err) {
        if (err) {
